66e2053801c18415ca432783ee2d950461162d1e
tangxianhai <tangxianhai@ireadercity.com>
2017-12-25 16:22:36 +0800

    fix directory problem

--- sudoapp.sh
+++ sudoapp.sh
@@ -1,8 +1,27 @@
 project=$(grep APPLICATION_NAME Makefile | awk '{print $3}')
 echo "creating root-permission script for: $project"
-mkdir layout && cd layout
-mkdir DEBIAN && cd DEBIAN && touch postinst && cd .. && cd .. && cd ..
-target=layout/DEBIAN/postinst
-sudo cp sudoapp.template $target
-perl -p -i -e "s,<ProjectName>,$project,g" $target
-chmod 0555 $target
+if [ ! -d "layout" ]; then
+    mkdir layout
+    cd layout
+ else
+ 	cd layout
+fi
+
+if [ ! -d "DEBIAN" ]; then
+    mkdir DEBIAN
+    cd DEBIAN
+ else
+ 	cd DEBIAN
+fi
+
+if [ -f "layout/DEBIAN/postinst" ]; then
+	echo "start copy postinst file to layout/DEBIAN/postinst , and please do code 'make do' again !"
+  	touch postinst && cd .. && cd ..
+	target=./layout/DEBIAN/postinst
+	sudo cp ./sudoapp.template $target
+	perl -p -i -e "s,<ProjectName>,$project,g" $target
+	chmod 0555 $target
+else
+	echo "layout/DEBIAN/postinst file exists and do nothing"
+fi
+
